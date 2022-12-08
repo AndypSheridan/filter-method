dbf8c9423cac3bf9c22bcacc6d0c69eb17054231
AndypSheridan <andy.p.sheridan@gmail.com>
2022-12-08 17:59:30 +0000

    Add CI Challenge

--- filter.js
+++ filter.js
@@ -21,6 +21,16 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(person => person.name === "Paul");
+console.log(paul);
+
+// Return only object, not as array
+
+const paul2 = people.filter(person => person.name === "Paul")[0];
+console.log(paul2);
 
 // Complex Filtering
 const students = [
@@ -55,3 +65,46 @@ const students = [
     ]
   },
 ];
+
+const hasStrongSkills = (student => {
+  let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5)
+  return strongSkills.length > 0;
+});
+
+const candidates = students.filter(hasStrongSkills);
+
+console.log(candidates);
+
+
+// CI Filter Challenge
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+const topMaths = students.filter(student => student.results.maths >= 90);
+
+console.log(topMaths);
